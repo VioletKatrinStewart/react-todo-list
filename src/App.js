72a6a0b735544074c5060e07649f4fb711bc3bde
Violet Stewart <vkazoo2@gmail.com>
2022-01-11 14:51:13 -0800

    got authentication working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { getUser, logout } from './services/users';
+import Auth from './views/Auth';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
+  const logoutUser = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {currentUser && (
+              <>
+                <h1>you are currently logged in</h1>
+                <button onClick={logoutUser}>sign out</button>
+              </>
+            )}
+            {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/views/Auth.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { signInUser, signUpUser } from '../services/users';
+import AuthForm from '../components/AuthForm';
+import classNames from 'classnames';
+import './Auth.css';
+
+export default function Auth({ setCurrentUser }) {
+  const [type, setType] = useState('signin');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let resp;
+      if (type === 'signin') {
+        resp = await signInUser(email, password);
+      } else {
+        resp = await signUpUser(email, password);
+      }
+      setCurrentUser(resp);
+    } catch {
+      setErrorMessage('Something went wrong. Please try again.');
+    }
+  };
+
+  return (
+    <div>
+      <div className="tabs">
+        <h3
+          onClick={() => {
+            setType('signin');
+          }}
+          className={classNames({ active: type === 'signin' })}
+        >
+          Sign In
+        </h3>
+
+        <h3
+          onClick={() => {
+            setType('signup');
+          }}
+          className={classNames({ active: type === 'signup' })}
+        >
+          Sign Up
+        </h3>
+      </div>
+      <AuthForm
+        errorMessage={errorMessage}
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
+}
